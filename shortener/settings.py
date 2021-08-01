854e8ddcb44f6fb1d7b8f55b1213400c660d09b7
rohinirg <rohinirao2112@gmail.com>
2021-08-01 14:44:48 +0530

    change in settings

--- shortener/settings.py
+++ shortener/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$0_wax^anl@59ktq@$x$tsw9%o((&nng2aqnpa=^kx%rt@0zj&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
